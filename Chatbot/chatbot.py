8bdef1cb82f6ab82bfadd660b5b6aa2b53e89cf0
Chris Jones <chris@christopherjones.us>
2012-11-02 08:00:14 -0400

    Added the sites I've been using as reference for next time.

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -1,3 +1,12 @@
+'''
+
+These are the websites that I've been referencing:
+http://blog.initprogram.com/2010/10/14/a-quick-basic-primer-on-the-irc-protocol/
+http://colinhorne.blogspot.com/2005/01/irc-protocol.html
+http://docs.python.org/release/2.6/
+
+'''
+
 class chatbot():
     pass    
 
